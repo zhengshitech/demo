c7ece824b527c58590246f9a25f2bac4338c5365
qiang.zheng <zq@sxw.cn>
2017-01-10 21:42:51 +0800

    temp_save

--- spring-demos/spring-demo-eureka/client-subject/src/main/java/tech/zhengshi/SpringDemoSubjectController.java
+++ spring-demos/spring-demo-eureka/client-subject/src/main/java/tech/zhengshi/SpringDemoSubjectController.java
@@ -1,6 +1,10 @@
 package tech.zhengshi;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by zhengqiang on 2017/1/10 12:43.
  * desc:
  */
+@Configuration
+@EnableAutoConfiguration
 @RestController
 public class SpringDemoSubjectController {
     @Value("${words}") String words;
 
-    @RequestMapping("/")
+    @RequestMapping("/ssss")
     public  String getWord() {
         String[] wordArray = words.split(",");
         int i = (int)Math.round(Math.random() * (wordArray.length - 1));
         return wordArray[i];
     }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoSubjectController.class, args);
+    }
+
 }
--- spring-demos/spring-demo-eureka/client-subject/src/main/java/tech/zhengshi/SpringDemoSubjectEurekaClientApplication.java
+++ spring-demos/spring-demo-eureka/client-subject/src/main/java/tech/zhengshi/SpringDemoSubjectEurekaClientApplication.java
@@ -1,11 +1,14 @@
 package tech.zhengshi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
-@EnableEurekaClient
 public class SpringDemoSubjectEurekaClientApplication {
 
 	public static void main(String[] args) {
