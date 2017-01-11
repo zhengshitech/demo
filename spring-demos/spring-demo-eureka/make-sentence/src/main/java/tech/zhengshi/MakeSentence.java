492933ce7e2f8f5c03512273fc27ccd624bdf85b
z <z@zhengshi.tech>
2017-01-11 08:15:48 +0800

    Ribbon 轮询式负载均衡完成

--- spring-demos/spring-demo-eureka/make-sentence/src/main/java/tech/zhengshi/MakeSentence.java
+++ spring-demos/spring-demo-eureka/make-sentence/src/main/java/tech/zhengshi/MakeSentence.java
@@ -3,6 +3,7 @@ package tech.zhengshi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,8 +18,10 @@ import java.util.List;
 @RestController
 public class MakeSentence {
 
+    //    @Autowired
+//    DiscoveryClient client;
     @Autowired
-    DiscoveryClient client;
+    LoadBalancerClient client;
 
     @RequestMapping("/sentence")
     public String getSentence() {
@@ -28,17 +31,28 @@ public class MakeSentence {
         System.out.println(sentence);
         return sentence;//大小写不区分
     }
+//
+//    public String getWord(String service) {
+//        List<ServiceInstance> list = client.getInstances(service);
+//        if (list != null && list.size() > 0) {
+//            ServiceInstance serviceInstance = list.get(0);
+//            URI uri = serviceInstance.getUri();
+//            if (uri != null) {
+//                String forObject = (new RestTemplate()).getForObject(uri, String.class);
+//                return forObject;
+//            }
+//        }
+//        return null;
+//    }
+
 
     public String getWord(String service) {
-        List<ServiceInstance> list = client.getInstances(service);
-        if (list != null && list.size() > 0) {
-            ServiceInstance serviceInstance = list.get(0);
+            ServiceInstance serviceInstance = client.choose(service);
             URI uri = serviceInstance.getUri();
             if (uri != null) {
                 String forObject = (new RestTemplate()).getForObject(uri, String.class);
                 return forObject;
             }
-        }
-        return null;
+            return "##### ERR MSG ####";
     }
 }
