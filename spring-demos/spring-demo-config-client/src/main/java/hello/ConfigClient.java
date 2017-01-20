b2f213d4515ade2de9ed682a68a82d7d232e67b6
qiang.zheng <zq@sxw.cn>
2017-01-20 18:43:43 +0800

    结合rabbit mq 动态刷新 properties文件的值

--- spring-demos/spring-demo-config-client/src/main/java/hello/ConfigClient.java
+++ spring-demos/spring-demo-config-client/src/main/java/hello/ConfigClient.java
@@ -2,6 +2,7 @@ package hello;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @EnableAutoConfiguration
 @RestController
+@RefreshScope
 public class ConfigClient {
 
     @Value("${lucky-word}")
