82bdc2f7e264ae2c7171951064d1f3d9f0125072
qiang.zheng <zq@sxw.cn>
2017-01-21 10:26:24 +0800

    Zuul API Gatewa 实验完成

--- spring-demos/spring-demo-eureka/make-sentence/src/main/java/tech/zhengshi/SpringDemoEurekaSentenceServerApplication.java
+++ spring-demos/spring-demo-eureka/make-sentence/src/main/java/tech/zhengshi/SpringDemoEurekaSentenceServerApplication.java
@@ -7,7 +7,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -16,9 +21,16 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableZuulProxy
 public class SpringDemoEurekaSentenceServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoEurekaSentenceServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoEurekaSentenceServerApplication.class, args);
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        System.err.println("do it");
+        return new ShallowEtagHeaderFilter();
+    }
 }
